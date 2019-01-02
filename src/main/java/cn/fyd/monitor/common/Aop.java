561e58976eecc6dfb1ed012d0cc9f7ae163d637a
fanyidong <fanyidong@biosan.cn>
2019-01-02 14:33:08 +0800

    修改AOP(@IsLogin修饰)增加验证是否处理本人数据

--- src/main/java/cn/fyd/monitor/common/Aop.java
+++ src/main/java/cn/fyd/monitor/common/Aop.java
@@ -23,8 +23,9 @@ import static cn.fyd.common.Constant.*;
 public class Aop {
 
     /**
-     * 验证用户是否登录
-     * 验证是否操作本人数据
+     * 使用IsLogin注解需要做的处理
+     * 功能点:1、验证用户是否登录
+     *       2、验证是否操作本人数据
      * @param pjp 连接点
      * @param @IsLogin 使用对应注解(@IsLogin)的方法才进行验证
      * @return 通用格式返回
@@ -33,31 +34,37 @@ public class Aop {
     @Around("@annotation(cn.fyd.annotation.IsLogin)")
     public Object checkLogin(ProceedingJoinPoint pjp) throws Throwable {
         Object[] args = pjp.getArgs();
-        int order=-1;
+        int orderOfHttpServletRequest=-1;
         for (int i=0; i<args.length; i++) {
             // 判断参数中是否存在HttpServletRequest类型的参数
             if (args[i] instanceof HttpServletRequest) {
-                order=i;
+                orderOfHttpServletRequest = i;
             }
         }
         // 如果没有HttpServletRequest类型参数则不需要验证是否登录
-        if (order==-1) {
+        if (orderOfHttpServletRequest==-1) {
             return Response.failed(WRONG_DATA).toString();
         }
-        HttpServletRequest request = (HttpServletRequest) args[order];
+        HttpServletRequest request = (HttpServletRequest) args[orderOfHttpServletRequest];
+        // 获取session
         HttpSession session = request.getSession();
-        if (session.getAttribute(USER_BEAN)==null) {
+        // 若session中没有userBean变量则报用户未登录的错
+        User userBean = (User) session.getAttribute(USER_BEAN);
+        if (userBean == null) {
             return Response.failed(USER_NOT_LOGIN).toString();
         }
-        // 获取session中的用户信息
-        User userBean = (User) request.getSession().getAttribute(USER_BEAN);
-        // 获取第一个参数
-        Object param = args[0];
+        // 获取参数列表中的第一个参数
+        Object firstParam = args[0];
         String userId = null;
-        if (param instanceof String) {
-            userId = (String) param;
-        } else if (param instanceof User) {
-            userId = ((User) param).getUserId();
+        // 给userId赋值
+        if (firstParam instanceof String) {
+            // 查询用户个人信息接口(/userInfo)
+            userId = (String) firstParam;
+        } else if (firstParam instanceof User) {
+            // 修改用户个人信息接口(/edit)
+            userId = ((User) firstParam).getUserId();
+        } else {
+            // 待补充
         }
         // 验证是否是登录用户操作本人数据
         if (!userBean.getUserId().equals(userId)) {
