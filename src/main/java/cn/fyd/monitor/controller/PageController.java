379056ad3b41142539fce0ec4d0d2a60c0308bc0
fanyidong <805334683@qq.com>
2019-01-17 13:08:25 +0800

    修改页面跳转控制层

--- src/main/java/cn/fyd/monitor/controller/PageController.java
+++ src/main/java/cn/fyd/monitor/controller/PageController.java
@@ -1,17 +1,23 @@
 package cn.fyd.monitor.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
+ * 获取页面的控制层
  * @author fanyidong
  * @date Created in 2019-01-14
  */
 @Controller
 public class PageController {
 
-    @RequestMapping("/")
-    public String getPage(String page) {
+    @GetMapping("/index")
+    public String getIndex(String page) {
+        return page;
+    }
+
+    @GetMapping("/regist")
+    public String getRegist(String page) {
         return page;
     }
 }
