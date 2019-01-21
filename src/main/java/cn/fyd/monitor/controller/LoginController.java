615d406d169dd60a979066328b14942d70b48311
fanyidong <805334683@qq.com>
2019-01-21 14:13:49 +0800

    编辑用户信息接口增加userId为空的验证

--- src/main/java/cn/fyd/monitor/controller/LoginController.java
+++ src/main/java/cn/fyd/monitor/controller/LoginController.java
@@ -9,6 +9,7 @@ import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -75,6 +76,10 @@ public class LoginController {
     @IsLogin
     @PostMapping("/edit")
     public String edit(User newUser, HttpServletRequest request) {
+        // 验证部分参数是否为空
+        if (StringUtils.isEmpty(newUser.getUserId())) {
+            return Response.failed(EMPTY_PARAMS).toString();
+        }
         Response res = loginRemote.edit(newUser);
         return res.toString();
     }
