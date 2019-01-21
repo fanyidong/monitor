04bf9dfd54e4bb44a9ae091a38611b098958f378
fanyidong <805334683@qq.com>
2019-01-21 16:45:27 +0800

    增加session过滤器

--- src/main/java/cn/fyd/monitor/MonitorApplication.java
+++ src/main/java/cn/fyd/monitor/MonitorApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -15,6 +16,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@ServletComponentScan
 public class MonitorApplication {
 
     private static Logger log = LoggerFactory.getLogger(MonitorApplication.class);
