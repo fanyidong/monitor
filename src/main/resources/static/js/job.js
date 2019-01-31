25b2ed48f62c56f6690ee9630433e4f211eb2159
fanyidong <805334683@qq.com>
2019-01-31 10:50:02 +0800

    任务列表操作栏添加操作按钮

--- src/main/resources/static/js/job.js
+++ src/main/resources/static/js/job.js
@@ -29,36 +29,30 @@ function createShowingTable(res, whereDo){
     }
     //此处需要让其动态的生成一个table并填充数据
     var tableStr = "<table class='table'>";
-    // whereDo=1表示main页面，其他表示manage页面
-    if (whereDo == 1) {
-        tableStr = tableStr + "<thead class='text-primary'><th>监控任务名</th><th>监控地址</th><th>监控频率(分钟)</th><th>警告方式</th><th>状态</th>";
-    } else {
-        tableStr = tableStr + "<thead class='text-primary'><th>监控任务名</th><th>监控地址</th><th>监控频率(分钟)</th><th>监控类型</th><th>警告方式</th><th>状态</th><th>操作</th>";
+    // table表头
+    tableStr = tableStr + "<thead class='text-primary'><th>监控任务名</th><th>监控地址</th><th>监控频率(分钟)</th><th>监控类型</th><th>警告方式</th><th>状态</th>";
+    if (whereDo !== 1) {
+        tableStr += "<th>操作</th>";
     }
     // 若返回结果不为空则填充数据
     if (dataArray != null && len > 0) {
-        if (whereDo == 1) {
-            for (var i = 0; i < len; i++) {
-                tableStr = tableStr + "<tbody><tr>"
-                    + "<td>" + dataArray[i].name + "</td>"
-                    + "<td>" + dataArray[i].url + "</td>"
-                    + "<td>" + dataArray[i].watchTime + "</td>"
-                    + "<td>" + transformWarnMethod(dataArray[i].warnMethod) + "</td>"
-                    + "<td>" + transformState(dataArray[i].state) + "</td>"
-                    + "</tr></tbody>";
-            }
-        } else {
-            for (var i = 0; i < len; i++) {
-                tableStr = tableStr + "<tbody><tr>"
-                    + "<td>" + dataArray[i].name + "</td>"
-                    + "<td>" + dataArray[i].url + "</td>"
-                    + "<td>" + dataArray[i].watchTime + "</td>"
-                    + "<td>" + transformType(dataArray[i].type) + "</td>"
-                    + "<td>" + transformWarnMethod(dataArray[i].warnMethod) + "</td>"
-                    + "<td>" + transformState(dataArray[i].state) + "</td>"
-                    + "<td>" + dataArray[i].watchTime + "</td>"
-                    + "</tr></tbody>";
-            }
+        for (var i = 0; i < len; i++) {
+            tableStr += "<tbody><tr>"
+                + "<td>" + dataArray[i].name + "</td>"
+                + "<td>" + dataArray[i].url + "</td>"
+                + "<td>" + dataArray[i].watchTime + "</td>"
+                + "<td>" + transformType(dataArray[i].type) + "</td>"
+                + "<td>" + transformWarnMethod(dataArray[i].warnMethod) + "</td>"
+                + "<td>" + transformState(dataArray[i].state) + "</td>";
+                // whereDo=1表示main页面，其他表示manage页面
+                if (whereDo !== 1) {
+                    tableStr += "<td>"
+                        + "<a href='' style='font-weight: bolder'>编辑</a>\t"
+                        + "<a href='' style='font-weight: bolder'>" + transformStateReverse(dataArray[i].state) + "</a>\t"
+                        + "<a href='' style='font-weight: bolder'>删除</a>"
+                        + "</td>"
+                }
+            tableStr += "</tr></tbody>";
         }
     }
     tableStr = tableStr + "</table>";
@@ -72,7 +66,7 @@ function createShowingTable(res, whereDo){
 }
 
 
-// 警告方式（1邮件 2手机）字段转换
+// 警告方式(1邮件 2手机)字段转换
 function transformWarnMethod(number) {
     if (number===1) {
         return "邮件";
@@ -81,7 +75,7 @@ function transformWarnMethod(number) {
     }
 }
 
-// 启用状态（0禁用 1启用）字段转换
+// 启用状态(0禁用 1启用)字段转换
 function transformState(number) {
     if (number===1) {
         return "启用";
@@ -90,7 +84,7 @@ function transformState(number) {
     }
 }
 
-// 监控类型（1网站 2接口 3服务）字段转换
+// 监控类型(1网站 2接口 3服务)字段转换
 function transformType(number) {
     if (number===1) {
         return "网站";
@@ -98,5 +92,13 @@ function transformType(number) {
         return "接口"
     } else if (number===3) {
         return "服务"
+    }
+}
+// 操作字段转换(与启用状态相反)
+function transformStateReverse(number) {
+    if (number===0) {
+        return "启用";
+    } else if (number===1) {
+        return "禁用"
     }
 }
